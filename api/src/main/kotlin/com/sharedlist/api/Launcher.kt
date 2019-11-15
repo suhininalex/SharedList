523019687b6933350b60a4b761582fbf6a19971a
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2019-11-15 21:56:13 +0200

    add cors header

--- api/src/main/kotlin/com/sharedlist/api/Launcher.kt
+++ api/src/main/kotlin/com/sharedlist/api/Launcher.kt
@@ -1,8 +1,29 @@
 package com.sharedlist.api
 
 import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder
+import java.util.UUID
+
+data class Item(val name: String)
 
 fun main() {
-    val app = Javalin.create().start(7000)
-    app.get("/") { ctx -> ctx.result("Hello World") }
+    val app = Javalin.create().apply {
+        config.enableCorsForAllOrigins()
+    }
+    app.routes {
+        ApiBuilder.get("/api/items") { context ->
+            context.json(listOf(Item("Сакхар"), Item("Сало")))
+        }
+
+        ApiBuilder.get("/") { context ->
+            context.result("Go duck yourself")
+        }
+    }
+    app.start(8080)
+}
+
+data class ItemList private constructor(val id: String, val items: MutableList<Item>){
+    companion object {
+        fun create() = ItemList(UUID.randomUUID().toString(), ArrayList())
+    }
 }
