4c27af989d14e44a833fca5ec87b4555f03a7b01
KoTDII <iiikotdiii@gmail.com>
2019-11-16 23:08:32 +0300

    add title change

--- ui/src/lists/thunks.js
+++ ui/src/lists/thunks.js
@@ -1,13 +1,16 @@
-import { listFetchInit, listFetchSuccess, listFetchFailure, listAddItem, listRemoveItem } from './actions'
+import { listFetchInit, listFetchSuccess, listFetchFailure, listAddItem, listRemoveItem } from './actions';
 import { url } from './consts';
-import { call } from 'libs/api-helpers'
+import { call } from 'libs/api-helpers';
 // import { fetchListMock } from './mocks'
 
 export const getList = (id) => (dispatch) => {
     dispatch(listFetchInit());
 
     call(`${url}/lists/${id}`, 'GET', {}, data => data)
-        .then(response => dispatch(listFetchSuccess(response)))
+        .then(response => {
+            dispatch(listFetchSuccess(response));
+            window.document.title = response.name;
+        })
         .catch(error => dispatch(listFetchFailure(error)))
 }
 
