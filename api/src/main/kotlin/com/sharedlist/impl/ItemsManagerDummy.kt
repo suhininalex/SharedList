3f498222916e5776eecd90df7f31242265e81827
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2019-11-16 13:31:09 +0200

    add in-memory implementation

--- api/src/main/kotlin/com/sharedlist/impl/ItemsManagerDummy.kt
+++ api/src/main/kotlin/com/sharedlist/impl/ItemsManagerDummy.kt
@@ -5,27 +5,39 @@ import com.sharedlist.api.ItemList
 import com.sharedlist.api.ItemsManager
 
 class ItemsManagerDummy: ItemsManager {
+    private val listMap = HashMap<String, ItemList>()
+
+    init {
+        val test = ItemList(id = "test", name = "Test list", items = listOf(Item("Сало"), Item("Сахар")))
+        listMap[test.id] = test
+    }
+
     override fun create(name: String): ItemList {
-        return ItemList(UIDGenerator.getID(), name, emptyList())
+        val itemList = ItemList(UIDGenerator.getID(), name, emptyList())
+        listMap[itemList.id] = itemList
+        return itemList
     }
 
     override fun rename(listId: String, name: String) {
-
+        val itemList = listMap[listId] ?: return
+        listMap[listId] = itemList.copy(name = name)
     }
 
-    override fun find(listId: String): ItemList {
-        return ItemList(listId, "List name", listOf(Item("Orange"), Item("Lemon")))
+    override fun find(listId: String): ItemList? {
+        return listMap[listId]
     }
 
     override fun remove(listId: String) {
-
+        listMap.remove(listId)
     }
 
     override fun addItem(listId: String, item: Item) {
-
+        val itemList = listMap[listId] ?: return
+        listMap[listId] = itemList.copy(items = itemList.items + listOf(item))
     }
 
     override fun removeItem(listId: String, itemId: String) {
-
+        val itemList = listMap[listId] ?: return
+        listMap[listId] = itemList.copy(items = itemList.items.filterNot { it.name == itemId })
     }
 }
